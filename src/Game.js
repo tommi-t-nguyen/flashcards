c892150a0353b9713cbcb9ef6945b3407c64e740
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-12-02 18:10:32 -0500

    Complete Game test and added a timer function

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,39 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+    this.timeStart = 0;
+  }
 
   printMessage(deck, round) {
-    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+   let cards = prototypeQuestions.map(cardInfo => {
+     return new Card(
+       cardInfo.id,
+       cardInfo.question,
+       cardInfo.answers,
+       cardInfo.correctAnswer);
+   });
+   const deck = new Deck(cards);
+   this.currentRound = new Round(deck);
+   this.currentRound.startTime = Date.now()
+   this.printMessage(deck, this.currentRound);
+   this.printQuestion(this.currentRound);
+ }
 }
 
 module.exports = Game;
